1285ce4d6a5bcb03defeec89415aadfed1f1a30d
vladbusov <to2002to2002@gmail.com>
2019-10-21 23:47:12 +0300

    travis1

--- Counter/counter.h
+++ Counter/counter.h
@@ -2,9 +2,9 @@
 // Created by vlad on 21.10.2019.
 //
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #ifndef DZ2_COUNTER_H
 #define DZ2_COUNTER_H
@@ -13,18 +13,14 @@
 // Created by vlad on 20.10.2019.
 //
 
-
-
-
 #define SIZE_OF_CHUNK 1000000
 
 int min(int a, int b);
-void charCounter(char* chars, int* cntChars, const char* buffer, int begin, int end);
+void charCounter(char* chars, int* cntChars, const char* buffer, int begin,
+                 int end);
 void readPipe(int* sharedCnt, int size, int* fd);
 void writePipe(char* countBuff, char* buffer, int* fd, int pid, int i);
 void interProcessCounter(char* buffer, char* countBuff, int* sharedCnt);
 void printResult(int* sharedCnt, int size);
 
-
-
-#endif //DZ2_COUNTER_H
+#endif  // DZ2_COUNTER_H
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,65 +1,66 @@
-#include <iostream>
+#include <gtest/gtest.h>
 #include <stdio.h>
 #include <cstring>
 #include <ctime>
-#include <gtest/gtest.h>
+#include <iostream>
 
 extern "C" {
-    #include "../trivialCounter/trivialCounter.h"
-    #include "../Counter/counter.h"
+#include "../Counter/counter.h"
+#include "../trivialCounter/trivialCounter.h"
 }
 
 #define FILESIZE 104857600
 
 class TestSerialization : public ::testing::Test {
-public:
-    TestSerialization() {
-        FILE* mf=fopen ("test.txt","w");
+ public:
+  TestSerialization() {
+    FILE* mf = fopen("test.txt", "w");
 
-        srand(time(NULL));
+    srand(time(NULL));
 
-        for (int i = 0; i < FILESIZE; i++) {
-            char ch = rand()%255;
-            fputc(ch, mf);
-        }
-
-        fclose(mf);
+    for (int i = 0; i < FILESIZE; i++) {
+      char ch = rand() % 255;
+      fputc(ch, mf);
     }
-    ~TestSerialization() { /* free protected members here */ }
-    void SetUp() { /* called before every test */ }
-    void TearDown() { /* called after every test */ }
 
-protected:
-    /* none yet */
+    fclose(mf);
+  }
+  ~TestSerialization() { /* free protected members here */
+  }
+  void SetUp() { /* called before every test */
+  }
+  void TearDown() { /* called after every test */
+  }
+
+ protected:
+  /* none yet */
 };
 
 TEST_F(TestSerialization, testRealization) {
-    FILE* mf=fopen ("test.txt","r");
-    char *buffer= new char[FILESIZE];
-    fgets(buffer, FILESIZE, mf);
-    fclose(mf);
+  FILE* mf = fopen("test.txt", "r");
+  char* buffer = new char[FILESIZE];
+  fgets(buffer, FILESIZE, mf);
+  fclose(mf);
 
-    char countBuff[] = "Vlad Busov";
+  char countBuff[] = "Vlad Busov";
 
-    int* result1 = new int[strlen(countBuff)];
-    memset(result1, 0, sizeof(int)*strlen(countBuff));
-    int* result2 = new int[strlen(countBuff)];
-    memset(result2, 0, sizeof(int)*strlen(countBuff));
+  int* result1 = new int[strlen(countBuff)];
+  memset(result1, 0, sizeof(int) * strlen(countBuff));
+  int* result2 = new int[strlen(countBuff)];
+  memset(result2, 0, sizeof(int) * strlen(countBuff));
 
+  charCount(countBuff, buffer, result1);
+  interProcessCounter(buffer, countBuff, result2);
 
-    charCount(countBuff, buffer, result1);
-    interProcessCounter(buffer, countBuff, result2);
+  EXPECT_TRUE(0 ==
+              std::memcmp(result1, result2, strlen(countBuff) * sizeof(int)));
 
-    EXPECT_TRUE( 0 == std::memcmp( result1, result2, strlen(countBuff)*sizeof(int) ) );
-
-    delete[] buffer;
-    delete[] result1;
-    delete[] result2;
+  delete[] buffer;
+  delete[] result1;
+  delete[] result2;
 }
 
-
-
 int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
