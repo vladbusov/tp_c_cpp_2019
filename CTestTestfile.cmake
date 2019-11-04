559bfd51b94e5b20a73de0df8cfd636c91e7a21b
vladbusov <to2002to2002@gmail.com>
2019-11-05 00:01:06 +0300

    cmake 3.12 fix3

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vladik/CLionProjects/tp_c_cpp_2019
+# Build directory: /home/vladik/CLionProjects/tp_c_cpp_2019
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
