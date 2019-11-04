0f1254657677456557f0690e121ca4fceb8c4a56
vladbusov <to2002to2002@gmail.com>
2019-11-05 00:41:06 +0300

    fix calloc

--- Counter/counter.c
+++ Counter/counter.c
@@ -1,16 +1,13 @@
-//
-// Created by vlad on 21.10.2019.
-//
-
-//
-// Created by vlad on 20.10.2019.
-//
 #include "counter.h"
 
 unsigned long min(const unsigned long a, const unsigned long b) { return (a < b) ? a : b; }
 
 void char_counter(char* chars, int* cnt_chars, const char* buffer, const unsigned long begin,
                   const unsigned long end) {
+    if (!chars || !cnt_chars || !buffer) {
+        fprintf(stderr, "nullptr, exiting \n");
+        exit(EXIT_FAILURE);
+    }
     int size_chars = (int)strlen(chars);
     for (unsigned long i = begin; i <= end; i++) {
         for (int j = 0; j < size_chars; j++) {
@@ -21,18 +18,25 @@ void char_counter(char* chars, int* cnt_chars, const char* buffer, const unsigne
 }
 
 void read_pipe(int* shared_cnt, int size, int* fd) {
-    int* buf;
-    int errflag;
-
+    if (!shared_cnt || !fd) {
+        fprintf(stderr, "nullptr, exiting \n");
+        exit(EXIT_FAILURE);
+    }
     long l1dcls = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
 
     if (unlikely(l1dcls == -1))
         l1dcls = sizeof(void*);
 
-    errflag = posix_memalign((void**)&buf, l1dcls, sizeof(int)*size);
+    int* buf = NULL;
+    int errflag = posix_memalign((void**)&buf, l1dcls, sizeof(int)*size);
 
     if (unlikely(errflag != 0))
         buf = calloc(size, sizeof(int));
+
+    if (!buf) {
+        fprintf(stderr, "mem failure, exiting \n");
+        exit(EXIT_FAILURE);
+    }
 
     close(fd[1]);
     read(fd[0], buf, sizeof(int)*size);
@@ -45,7 +49,15 @@ void read_pipe(int* shared_cnt, int size, int* fd) {
 }
 
 void write_pipe(char* count_buff, char* buffer, int* fd, int i, const unsigned long sizeOfChunk) {
+    if (!count_buff || !fd || !buffer) {
+        fprintf(stderr, "nullptr, exiting \n");
+        exit(EXIT_FAILURE);
+    }
     int* cnt = (int*)calloc(strlen(count_buff), sizeof(int));
+    if (!cnt) {
+        fprintf(stderr, "mem failure, exiting \n");
+        exit(EXIT_FAILURE);
+    }
 
     char_counter(count_buff, cnt, buffer, sizeOfChunk * (long)i,
                  min(sizeOfChunk * (i + 1), (int) strlen(buffer)) - 1);
@@ -64,14 +76,27 @@ void write_pipe(char* count_buff, char* buffer, int* fd, int i, const unsigned l
 }
 
 void prl_char_counter(char* count_buff, char* buffer, int* shared_cnt) {
+    if (!count_buff|| !shared_cnt || !buffer) {
+        fprintf(stderr, "nullptr, exiting \n");
+        exit(EXIT_FAILURE);
+    }
     long numOfProcesses = sysconf(_SC_NPROCESSORS_ONLN) * 2 + 1;
 
     const unsigned long sizeOfChunk = (long)strlen(buffer) / numOfProcesses + 1;
 
 
     int** fd = (int**)calloc(numOfProcesses, sizeof(int*));
+    if (!fd) {
+        fprintf(stderr, "mem failure, exiting \n");
+        exit(EXIT_FAILURE);
+    }
+
     for (long i = 0; i < numOfProcesses; i++) {
         fd[i] = (int*)calloc(2, sizeof(int));
+        if (!fd[i]) {
+            fprintf(stderr, "mem failure, exiting \n");
+            exit(EXIT_FAILURE);
+        }
     }
 
     pid_t current = 0;
