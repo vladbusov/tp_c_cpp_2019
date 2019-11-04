712c679a8202bde34ea1ffcc3b522cd415e7ce74
vladbusov <to2002to2002@gmail.com>
2019-11-04 21:43:53 +0300

    cpp check travis fix

--- production/maketxt.cpp
+++ production/maketxt.cpp
@@ -1,30 +1,30 @@
 #include <cstring>
 #include <ctime>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 #define FILESIZE 104857600
 
+using std::exception;
 using std::ifstream;
 using std::ofstream;
 using std::string;
-using std::exception;
 
 void makeTestFile(const string& fileName) {
-    ofstream testFile(fileName);
+  ofstream testFile(fileName);
 
-    unsigned int seed = 0;
-    srand(time(NULL));
+  unsigned int seed = 0;
+  srand(time(NULL));
 
-    for (int i = 0; i < FILESIZE; i++) {
-        char ch = (rand_r(&seed) % 94) + 32;
-        testFile << ch;
-    }
-    testFile.close();
+  for (int i = 0; i < FILESIZE; i++) {
+    char ch = (rand_r(&seed) % 94) + 32;
+    testFile << ch;
+  }
+  testFile.close();
 }
 
 int main(int argc, char** argv) {
-    if (argc < 2) throw exception();
-    makeTestFile(argv[1]);
-    return 0;
+  if (argc < 2) throw exception();
+  makeTestFile(argv[1]);
+  return 0;
 }
